a704ce845aa12563d27422ddd427f7fa3d499931
crislapi1 <159267181+crislapi1@users.noreply.github.com>
2024-02-15 17:37:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from database.creazione_tabelle_db import  *
 
 if __name__ == '__main__':
     crea_db()
-    crea_tabelle_sql(r"C:\pythonSac\querysql\daitv_finale.sql")
+    crea_tabelle_sql(r"C:\Users\lapia\OneDrive\Desktop\ESERCIZIO GIORGIO\pythonSac\querysql\daitv_finale.sql")
