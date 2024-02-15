1b2dd516ca1de5a0edc391648ef3542271add197
TriscariGiorgio <giorgiotriscari@gmail.com>
2024-02-15 17:04:39 +0100

    Create connessioni.py

---
+++ database/connessioni.py
@@ -0,0 +1,16 @@
+from funzioniDB import *
+from config import *
+
+def crea_db():
+    connection = create_server_connection(DB_HOST, DB_USER, DB_PASSWORD)
+    esegui_query(connection, f"DROP DATABASE {DB_NAME}")
+    create_database(connection, f"CREATE DATABASE {DB_NAME}")
+
+    return connection
+
+def crea_connessione_db():
+    connection = create_db_connection(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+    query = """SET GLOBAL max_allowed_packet = 268435456"""
+    cursor = connection.cursor()
+    cursor.execute(query)
+    return connection
